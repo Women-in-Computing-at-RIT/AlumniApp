d702aa6d50510cfd7a6c3b5520a4d9a3d269d4a5
Regina Locicero <rtl3971@rit.edu>
2017-02-23 14:41:02 -0500

    Fixed CreateAccount.js for basic functionality

--- AlumniApp/core/CreateAccount.js
+++ AlumniApp/core/CreateAccount.js
@@ -5,57 +5,61 @@
  */
 
 import React, {Component} from 'react';
-import{ AppRegistry, Text, TextInput, View} from 'react-native';
+import{ AppRegistry, Text, TextInput, View, Button, Alert} from 'react-native';
+
+// const example
+// const onPressButton = () => { Alert.alert('Button has been pressed!'); };
 
 /**
  *Made Constructor and its properties
  */
-class createAccount extends React.Component{
+export default class CreateAccount extends React.Component{
 	constructor(props){
 		super(props);
-		
+
 		this.state ={
-			fullName: '';
-			email:'';
-			password: '';
-			confirmPassword:'';
-		}	
+			fullName: '',
+			email:'',
+			password: '',
+			confirmPassword:''
+		}
+	}
+
+	// TODO - fix to push the entered information to realm etc.
+	onPressButton(){
+		Alert.alert('Button has been pressed!');
+	}
+
+	// Have the User input Name, E-mail, Password, Confirm Password
+	// Submit button changes state when it is pressed
+	render(){
+		return(
+			<View>
+				<TextInput
+					placeholder="Name"
+					onChangeText ={(text) => this.setState({fullName: text})}
+					value={this.state.fullName}
+				/>
+				<TextInput
+					placeholder="E-mail"
+					onChangeText ={(text)=>this.setState({email: text})}
+					value={this.state.email}
+				/>
+				<TextInput
+					placeholder="Password"
+					onChangeText={(text)=>this.setState({password: text})}
+					secureTextEntry={true}
+				/>
+				<TextInput
+					placeholder="Confirm Password"
+					onChangeText={(text)=>this.setState({confirmPassword: text})}
+					secureTextEntry={true}
+				/>
+				<Button onPress={this.onPressButton.bind(this)}
+				title = "Submit"
+				accessibilityLabel="Creating Account"
+				/>
+			</View>
+		);
 	}
 }
-
-/**
- *
- * Renders the fields
- * Have the User input Name, E-mail, Password, Confirm Password
- * Submit button changes state when it is pressed
- */
-render(){
-	return(
-		<View>
-			<TextInput
-				placeholder="Name"
-				onChangeText ={(text) => this.setState({Name: text})}
-				value{(this.state.name)}
-			/>
-			<TextInput
-				placeholder="E-mail"
-				onChangeText ={(text)=>this.setState({email: text})}
-				value{(this.state.email)}
-			/>
-			<TextInput
-				placeholder="Password"
-				onChangeText={(text)=>this.setState({password: text})
-				secureTextEntry={true}
-			/>
-			<TextInput
-				placeholder="Confirm Password"
-				onChangeText={(text)=>this.setState({confirmPassword: text})
-				secureTextEntry={true}
-			/>
-			<Button onPress={onPressButton}
-			title = "Submit"
-			accessibilityLabel="Creating Account"
-		/>
-		</View>
-	);
-};
