1024132451ccae4a91f8a181de515a2a870e9fdf
mytran <mt2915@rit.edu>
2017-03-02 15:57:53 -0500

    Added to Create Account

--- AlumniApp/core/CreateAccount.js
+++ AlumniApp/core/CreateAccount.js
@@ -4,127 +4,125 @@
  * Created by David on 02/16/17
  */
 
-import React, {Component} from 'react';
-<<<<<<< HEAD
-import{ AppRegistry, Text, TextInput, View, Alert} from 'react-native';
-=======
-import{ AppRegistry, Text, TextInput, View, Button, Alert} from 'react-native';
+import React, { Component } from 'react';
 
-// const example
-// const onPressButton = () => { Alert.alert('Button has been pressed!'); };
->>>>>>> refs/remotes/Women-in-Computing-at-RIT/master
+import { AppRegistry, Text, TextInput, View, Button, Alert } from 'react-native';
+
+
 
 /**
  *Made Constructor and its properties
  */
-export default class CreateAccount extends React.Component{
-	constructor(props){
+export default class CreateAccount extends React.Component {
+	constructor(props) {
 		super(props);
 
-		this.state ={
+		this.state = {
 			fullName: '',
-			email:'',
+			email: '',
 			password: '',
-			confirmPassword:''
+			confirmPassword: ''
 		}
 	}
-<<<<<<< HEAD
-
-}
-
-/**
- *
- * Renders the fields
- * Have the User input Name, E-mail, Password, Confirm Password
- * Submit button changes state when it is pressed
- */
-render(){
-	return(
-		<View>
-			<TextInput
-				placeholder="Name"
-				onChangeText ={(text) => this.setState({Name: text})}
-				value{(this.state.name)}
-				maxLength = {30}
-			/>
-			
-			<TextInput
-				placeholder="E-mail"
-				onChangeText ={(text)=>this.setState({email: text})}
-				value{(this.state.email)}
-				maxLength = {30}
-			/>
-			<TextInput
-				<Text>Please input a password at least 8 characters</Text>
-				<Text>To help make the password stronger use special characters or numbers</Text>
-				placeholder="Password"
-				onChangeText={(text)=>this.setState({password: text})
-				secureTextEntry={true}
-				maxLength = {30}
-				<ReactPasswordStrength
-					className="customStyling"
-					style={{ display: 'none' }}
-					minLength={8}
-					minScore={2}
-					scoreWords={['weak', 'okay', 'good', 'strong', 'excellent']}
-					changeCallback={foo}
-					inputProps={{ name: "password_input", autocomplete: "off" }}
-				/>
-			/>
-			<TextInput
-				placeholder="Confirm Password"
-				onChangeText={(text)=>this.setState({confirmPassword: text})
-				secureTextEntry={true}
-				maxLength = {30}
-			/>
-			<Button onPress={onPressButton}
-			title = "Submit"
-			accessibilityLabel="Creating Account"
-			if(Password.length < {8}){
-				alert("Password is not long enough!");
-			}
-		/>
-		</View>
-	);
-};
-=======
-
 	// TODO - fix to push the entered information to realm etc.
-	onPressButton(){
+	onPressButton() {
 		Alert.alert('Button has been pressed!');
 	}
 
-	// Have the User input Name, E-mail, Password, Confirm Password
-	// Submit button changes state when it is pressed
-	render(){
-		return(
+
+	/**
+	 *
+	 * Renders the fields
+	 * Have the User input Name, E-mail, Password, Confirm Password
+	 * Submit button changes state when it is pressed
+	 */
+	render() {
+		return (
 			<View>
 				<TextInput
 					placeholder="Name"
-					onChangeText ={(text) => this.setState({fullName: text})}
+					onChangeText={(text) => this.setState({ fullName: text })}
+					value{(this.state.name) }
+					maxLength={30}
+				/>
+
+				<TextInput
+					placeholder="E-mail"
+					onChangeText={(text) => this.setState({ email: text })}
+					value{(this.state.email) }
+					maxLength={30}
+				/>
+				<TextInput
+					placeholder="Password"
+					onChangeText={(text) => this.setState({ password: text })
+					secureTextEntry ={true}
+					maxLength={30}
+					<Text>Please input a password at least 8 characters</Text>
+			<Text>To help make the password stronger use special characters or numbers</Text>
+
+
+
+			< ReactPasswordStrength
+				className="customStyling"
+				style={{ display: 'none' }
+				}
+				minLength={8}
+				minScore={2}
+				scoreWords={['weak', 'okay', 'good', 'strong', 'excellent']}
+				changeCallback={foo}
+				inputProps={{ name: "password_input", autocomplete: "off" }}
+			/>
+		/>
+			<TextInput
+				placeholder="Confirm Password"
+				onChangeText={(text) => this.setState({ confirmPassword: text })
+						secureTextEntry={true}
+				maxLength={30}
+			/>
+			<Button onPress={onPressButton}
+				title="Submit"
+				accessibilityLabel="Creating Account"
+				if(Password.length < { 8}){
+			alert("Password is not long enough!");
+		}
+		/>
+			</View >
+		);
+	};
+
+
+
+
+	// Have the User input Name, E-mail, Password, Confirm Password
+	// Submit button changes state when it is pressed
+	render() {
+		return (
+			<View>
+				<TextInput
+					placeholder="Name"
+					onChangeText={(text) => this.setState({ fullName: text })}
 					value={this.state.fullName}
 				/>
 				<TextInput
 					placeholder="E-mail"
-					onChangeText ={(text)=>this.setState({email: text})}
+					onChangeText={(text) => this.setState({ email: text })}
 					value={this.state.email}
 				/>
 				<TextInput
 					placeholder="Password"
-					onChangeText={(text)=>this.setState({password: text})}
+					onChangeText={(text) => this.setState({ password: text })}
 					secureTextEntry={true}
 				/>
 				<TextInput
 					placeholder="Confirm Password"
-					onChangeText={(text)=>this.setState({confirmPassword: text})}
+					onChangeText={(text) => this.setState({ confirmPassword: text })}
 					secureTextEntry={true}
 				/>
 				<Button onPress={this.onPressButton.bind(this)}
-				title = "Submit"
-				accessibilityLabel="Creating Account"
+					title="Submit"
+					accessibilityLabel="Creating Account"
 				/>
 			</View>
 		);
 	}
 }
->>>>>>> refs/remotes/Women-in-Computing-at-RIT/master
