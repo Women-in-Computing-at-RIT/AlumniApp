7f9d8199e33d0053a7f724bae75747661c4e1e9f
dquach217 <dq6180@rit.edu>
2017-04-27 02:19:05 -0400

    Create the overall skeleton of the code

--- AlumniApp/www/js/app.js
+++ AlumniApp/www/js/app.js
@@ -62,33 +62,14 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         }
       })
 
-      .state('app.browse', {
-        url: '/browse',
+      .state('app.home', {
+        url: '/home',
         views: {
           'menuContent': {
-            templateUrl: 'templates/browse.html'
-          }
-        }
-      })
-      .state('app.playlists', {
-        url: '/playlists',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/playlists.html',
-            controller: 'PlaylistsCtrl'
-          }
-        }
-      })
-
-      .state('app.single', {
-        url: '/playlists/:playlistId',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/playlist.html',
-            controller: 'PlaylistCtrl'
+            templateUrl: 'templates/home.html'
           }
         }
       });
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/playlists');
+    $urlRouterProvider.otherwise('/app/home');
   });
--- AlumniApp/www/js/controllers.js
+++ AlumniApp/www/js/controllers.js
@@ -101,21 +101,6 @@ angular.module('starter.controllers', ['ui.rCalendar'])
     })
 
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
-
-
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-})
-
 .controller('CalendarDemoCtrl', function ($scope, $http) {
     'use strict';
     $scope.calendar = {};
